4568e38e7638fc4f1d92462d6260ba3a234a61de
Myo <myotm.18@gmail.com>
2019-05-30 18:24:30 +0800

    tutorial-six

--- node/controllers/authController.js
+++ node/controllers/authController.js
@@ -1,56 +1,63 @@
+var Promise = require('promise');
+var httpHelper = require('../helpers/http');
+var authHelper = require('../helpers/auth');
+
+
 exports.login = (req, res) => {
     var user = req.body;
-    console.log("Reached to login------------------"+user);
-    global.db.user.findOne({
-        email: user.email
-    }, (error, foundUser) => {
-        if (error) {
-            console.log("first Error-----");
-            res.status(500).json({
-                status: "Error",
-                error: error
-            });
-        } else {
-            if (!foundUser) {
-                console.log("second Error-----");
-                res.status(500).json({
-                    status: "ERROR",
-                    error: "Email is invalid."
-                });
+
+
+    var dbLogin = (fulfill, reject) => {
+        global.db.user.findOne({
+            email: user.email
+        }, (error, foundUser) => {
+            if (error) {
+                reject(error);
             } else {
-                if(user.password !== foundUser.password){
-                    console.log("third Error-----");
-                    res.status(500).json({
-                        status: "ERROR",
-                        error: "Password is invalid."
-                    });
-                }else{
-                    console.log("reached success----");
-                    res.status(200).json({
-                        status: "SUCCESS",
-                        data: foundUser
-                    });
+                if (!foundUser) {
+                    reject("Email is invalid.");
+                } else {
+                    if(user.password !== foundUser.password){
+                        reject("Password is invalid.")
+                    }else{
+                        fulfill(foundUser);
+                    }
                 }
             }
-        }
-    });
+        });
+    }
+
+    new Promise(dbLogin)
+        .then((user) => { 
+            return new Promise((fulfill, reject) => {
+                var token = authHelper.signUser(user.email);
+                user.credentials = token;
+                fulfill(user);
+            });
+         }, (error) => { httpHelper.resResult(res, error); })
+        .then((result) => { httpHelper.resResult(res, result); }, (error) => { httpHelper.resResult(res, error); });  
 }
+
+
 
 exports.signup = (req, res) => {
     var user = req.body;
-    console.log("reached to signup--------" + user);
-    global.db.user.insert(user, (err, result) => {
-        if (err) {
-            console.log("-----------ERR--------", err);
-            res.status(500).json({
-                status: "ERROR",
-                err: "Error while signup. DB ERROR."
-            });
-        } else {
-            res.status(200).json({
-                status: "SUCCESS",
-                data: result
-            });
-        }
+    
+    var dbSignup = (fulfill, reject) => {
+        global.db.user.insert(user, (err, result) => {
+            if (err) {
+                reject("Error while signup. Details: DB Error.");
+            } else {
+                fulfill(result);
+            }
+        });
+
+    }
+
+    new Promise(dbSignup).then((result) => {
+        httpHelper.resResult(res, result);
+    }, (error) => {
+        httpHelper.resResult(res, error);
     });
 }
+
--- node/routes/userRoute.js
+++ node/routes/userRoute.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var userController = require('../controllers/userController');
+var authHelper = require("../controllers/authController");
 
-router.post('/create', userController.create);
-router.get('/', userController.findAll);
-router.get('/username', userController.find);
-router.delete('/:username', userController.remove);
-router.put('/update', userController.update);
-
+router.post('/create', authHelper.authenticateRequest(), userController.create);
+router.get('/', authHelper.authenticateRequest(), userController.findAll);
+router.get('/username', authHelper.authenticateRequest(), userController.find);
+router.delete('/:username', authHelper.authenticateRequest(), userController.remove);
+router.put('/update', authHelper.authenticateRequest(), userController.update);
+router.put('/', authHelper.authenticateRequest(), userController.updateAll);
 
 module.exports = router;
